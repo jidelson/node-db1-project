f633616d0774743bc548ec4a731b4a1d9453ed9e
jidelson <58210030+jidelson@users.noreply.github.com>
2020-08-11 14:15:15 -0700

    attempted queries, get request working

--- api/server.js
+++ api/server.js
@@ -6,4 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+    res.send(`
+    <h2>Welcome to db 1!!!</h2>
+    `)
+  });
+
 module.exports = server;
